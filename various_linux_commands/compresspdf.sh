4465fc0cf3e547947d563737596d78daa5ae1e05
Suryanarayan Mondal <suryamondal@gmail.com>
2022-08-26 12:53:36 +0200

    Create compresspdf.sh
    This script compress pdf properly

---
+++ various_linux_commands/compresspdf.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script compress pdf by converting each pages into jpeg.
+# Arg 1: jpeg dpi
+# Arg 2: fileName
+
+filename=$2
+
+number=`pdftk LayerCompare.pdf dump_data | grep NumberOfPages | sed 's/[^0-9]*//'`
+echo 'number of pages: '$number
+
+pages=''
+pages1=''
+
+# convert each pages to jpeg and then to pdf
+for i in $(seq $number)
+do
+    printf -v j "%05d" $i
+
+    exec='pdftk A='$filename' cat A'$j' output '$j'.pdf' 
+    echo $exec
+    echo $exec | sh
+    
+    exec='convert -verbose -density '$1' -trim '$j'.pdf -quality 100 '$j'.jpg'
+    echo $exec
+    echo $exec | sh
+
+    exec='convert '$j'.jpg '$j'.pdf'
+    echo $exec
+    echo $exec | sh
+
+    pages+=' '$j'.pdf'
+    pages1+=' '$j'.jpg'
+done
+
+exec='pdftk '$pages' cat output output.pdf' 
+echo $exec
+echo $exec | sh
+
+exec='rm -rf '$pages' '$pages1
+echo $exec
+echo $exec | sh
